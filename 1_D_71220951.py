55481ee0fa8df3173a6eb688ae36bf0f4c777bc0
beatricedwiputri <117900796+beatricedwiputri@users.noreply.github.com>
2022-12-08 16:09:47 +0700

    Add files via upload

---
+++ 1_D_71220951.py
@@ -0,0 +1,23 @@
+Susu = int(20000)
+Permen = int(500)
+roti = int(15000)
+indomie = int(3000)
+vitamin = int(50000)
+
+masukinduit = input("Masukkan jumlah uang: ")
+ketikstart = input("Ketik 'START' untuk memulai:")
+
+apabarangyangdibeli = input("Apa barang yang akan anda beli?")
+
+if(ketikstart == "start".lower().upper()):
+    print(apabarangyangdibeli = input("Apa barang yang akan anda beli?"))
+elif(ketikstart == "StaRT"):
+    print(apabarangyangdibeli)
+else:
+    pass
+
+if(apabarangyangdibeli == Permen)
+
+
+
+
